e3501133f43e668be5f525979118bb4c8f2f9075
WillianZD <willianzhang44@gmail.com>
2025-10-05 23:47:29 -0300

    Criação da entidade Brinquedo

---
+++ src/main/java/com/example/loja_brinquedos/model/Brinquedo.java
@@ -0,0 +1,154 @@
+package com.example.loja_brinquedos.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+@Entity
+@Table(name = "brinquedos")
+public class Brinquedo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String codigo;
+
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private String nome;
+
+    @NotNull
+    @Column(nullable = false)
+    private BigDecimal valor;
+
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private String marca;
+
+    @NotNull
+    @NotBlank
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String descricao;
+
+    @NotNull
+    @NotBlank
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String detalhes;
+
+    @NotEmpty(message = "O brinquedo deve ter pelo menos uma categoria")
+    @ManyToMany
+    @JoinTable(
+            name = "produto_categoria",
+            joinColumns = @JoinColumn(name = "produto_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private Set<Categoria> categorias = new HashSet<>();
+
+    @NotEmpty
+    @OneToMany(mappedBy = "produto", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Imagem> imagens = new ArrayList<>();
+
+    @Column(nullable = false)
+    private Long views = 0L;
+
+    //Construtores
+    public Brinquedo() {}
+
+    public Brinquedo(String codigo, String nome, BigDecimal valor, String marca, String descricao, String detalhes, Set<Categoria> categorias, List<Imagem> imagens) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.valor = valor;
+        this.marca = marca;
+        this.descricao = descricao;
+        this.detalhes = detalhes;
+        this.categorias = categorias;
+        this.imagens = imagens;
+    }
+
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDetalhes() {
+        return detalhes;
+    }
+
+    public void setDetalhes(String detalhes) {
+        this.detalhes = detalhes;
+    }
+
+    public Set<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(Set<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+    public List<Imagem> getImagens() {
+        return imagens;
+    }
+
+    public void setImagens(List<Imagem> imagens) {
+        this.imagens = imagens;
+    }
+
+    public Long getViews() {
+        return views;
+    }
+
+    public void setViews(Long views) {
+        this.views = views;
+    }
+}
