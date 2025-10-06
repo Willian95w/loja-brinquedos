242c76e2e058817ec06ee2ec7d39b66f521e1a92
WillianZD <willianzhang44@gmail.com>
2025-10-06 02:18:58 -0300

    Adição da anotação de entity e criação da table

--- src/main/java/com/example/loja_brinquedos/model/Imagem.java
+++ src/main/java/com/example/loja_brinquedos/model/Imagem.java
@@ -3,6 +3,8 @@ package com.example.loja_brinquedos.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "imagens")
 public class Imagem {
 
     @Id
