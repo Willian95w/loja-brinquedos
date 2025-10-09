8e869e864f81ab3daacc03c697057a48f715d6f5
WillianZD <willianzhang44@gmail.com>
2025-10-09 10:09:40 -0300

    Correção na lógica de adição ou remoção de imagens de um brinquedo.

--- src/main/java/com/example/loja_brinquedos/service/BrinquedoService.java
+++ src/main/java/com/example/loja_brinquedos/service/BrinquedoService.java
@@ -210,7 +210,7 @@ public class BrinquedoService {
             }
         }
 
-        return brinquedoRepository.save(brinquedo);
+        return brinquedo;
     }
 
     public Brinquedo save(Brinquedo brinquedo) {
